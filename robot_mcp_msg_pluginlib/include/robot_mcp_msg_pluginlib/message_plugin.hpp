4a2e5c295c9c4ad098d47cfb59907a18a3fdfba5
Eddy Zhou <edsteredward@gmail.com>
2025-10-18 00:39:21 +0000

    pre-commit

--- robot_mcp_msg_pluginlib/include/robot_mcp_msg_pluginlib/message_plugin.hpp
+++ robot_mcp_msg_pluginlib/include/robot_mcp_msg_pluginlib/message_plugin.hpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2025-present WATonomous. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Copyright 2025 WATonomous
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,9 +29,11 @@
 #ifndef ROBOT_MCP_MSG_PLUGINLIB__MESSAGE_PLUGIN_HPP_
 #define ROBOT_MCP_MSG_PLUGINLIB__MESSAGE_PLUGIN_HPP_
 
-#include <string>
-#include <nlohmann/json.hpp>
 #include <rosidl_runtime_c/message_type_support_struct.h>
+
+#include <string>
+
+#include <nlohmann/json.hpp>
 
 namespace robot_mcp_msg_pluginlib
 {
